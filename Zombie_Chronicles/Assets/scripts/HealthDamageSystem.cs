3b88efdfa1ab675e647cd8d387e5715abb1d19f3
kanand003 <39569461+kanand003@users.noreply.github.com>
2019-10-18 22:01:08 +0530

    Adding

---
+++ Zombie_Chronicles/Assets/scripts/HealthDamageSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDamageSystem : MonoBehaviour
+{
+    public GameObject zombie;
+    public int startingHealth = 100;                            // The amount of health the player starts the game with.
+    public static int currentHealth;
+
+    // Update is called once per frame
+
+    void Awake()
+    {
+        startingHealth = currentHealth; 
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject==zombie)
+        {
+            startingHealth = startingHealth - 10;
+            Debug.Log(startingHealth);
+        }
+    }
+}
