329de05c0ecf90b4c4d9f97ccf8c9a5dc0ac9453
kanand003 <39569461+kanand003@users.noreply.github.com>
2019-11-01 22:09:03 +0530

    Sound Effect

--- Zombie_Chronicles/Assets/scripts/HealthDamageSystem.cs
+++ Zombie_Chronicles/Assets/scripts/HealthDamageSystem.cs
@@ -12,8 +12,9 @@ public class HealthDamageSystem : MonoBehaviour
     public int currentHealth;
     public SimpleHealthBar healthBar;
 
-    public Light2D L1,L2,L3,L4,L5,L6;
-
+    //public Light2D L1,L2,L3,L4,L5,L6;
+    public AudioSource A1;
+    public float time = 3f;
     // Update is called once per frame
 
 
@@ -22,9 +23,15 @@ public class HealthDamageSystem : MonoBehaviour
         startingHealth = 100;
         currentHealth = startingHealth;
         healthBar.UpdateBar(startingHealth,currentHealth);
+       // A1=GetComponent<AudioSource>();
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
+
+       // A1.Play();
+
+        //A1.SetScheduledStartTime(time);
+        //A1.SetScheduledEndTime(time + 3);
         if (startingHealth <= 0)
         {
             SceneManager.LoadScene("Lose");
@@ -40,46 +47,65 @@ public class HealthDamageSystem : MonoBehaviour
             Debug.Log(startingHealth);
             //GameObject light = GameObject.FindWithTag("Light");
             //L1 = light.GetComponent<Light2D>();
-            L1.enabled = false;
-            Debug.Log("L1");
+            //L1.enabled = false;
+            // Debug.Log("L1");
+           
         }
         
         else if (collision.gameObject == z1)
         {
             startingHealth = startingHealth - 10;
             Debug.Log(startingHealth);
+            A1.Play();
+            A1.SetScheduledStartTime(time);
+            //A1.SetScheduledEndTime(time + 3);
         }
         else if (collision.gameObject == z2)
         {
             startingHealth = startingHealth - 10;
             Debug.Log(startingHealth);
+            A1.Play();
+            A1.SetScheduledStartTime(time);
+            //A1.SetScheduledEndTime(time + 3);
         }
         else if (collision.gameObject == z3)
         {
             startingHealth = startingHealth - 10;
             Debug.Log(startingHealth);
+            A1.Play();
+            A1.SetScheduledStartTime(time);
+            //A1.SetScheduledEndTime(time + 3);
         }
         else if (collision.gameObject == z4)
         {
             startingHealth = startingHealth - 10;
             Debug.Log(startingHealth);
+            A1.Play();
+            A1.SetScheduledStartTime(time);
+            //A1.SetScheduledEndTime(time + 3);
             //if(L4.enabled==false)
             //{
-                L4.enabled = true;
-                Debug.Log("T");
-           // }
-            
-            Debug.Log("L4");
+            //L4.enabled = true;
+            // Debug.Log("T");
+            // }
+
+            //Debug.Log("L4");
         }
         else if (collision.gameObject == z5)
         {
             startingHealth = startingHealth - 10;
             Debug.Log(startingHealth);
+            A1.Play();
+            A1.SetScheduledStartTime(time);
+            //A1.SetScheduledEndTime(time + 3);
         }
         else if (collision.gameObject == z6)
         {
             startingHealth = startingHealth - 10;
             Debug.Log(startingHealth);
+            A1.Play();
+            A1.SetScheduledStartTime(time);
+            //A1.SetScheduledEndTime(time + 3);
         }
         healthBar.UpdateBar(100,startingHealth);
 
---
+++ Zombie_Chronicles/Assets/scripts/disappear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class disappear : MonoBehaviour
+{
+    public float time = 7.5f; //Seconds to read the text
+    //public GameObject gameObjec;
+    public Text T1,T2,T3;
+    void Start()
+    {
+        Destroy(T1, time);
+        Destroy(T2, time);
+        Destroy(T3, time);
+    }
+
+
+}
