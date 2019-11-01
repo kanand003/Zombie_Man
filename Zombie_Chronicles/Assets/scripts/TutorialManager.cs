4b6aa021bf3bf74c14738037ed3c1ee543b34c83
kanand003 <39569461+kanand003@users.noreply.github.com>
2019-11-01 20:36:50 +0530

    Tuts

---
+++ Zombie_Chronicles/Assets/scripts/TutorialManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+
+    
+    void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[popUpIndex].SetActive(true);
+            }
+            else
+            {
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
+        if(popUpIndex==0)
+    }
+}
