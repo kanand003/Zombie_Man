3cd577dc091c380dea9ebcc5b578e06429be6b65
kanand003 <39569461+kanand003@users.noreply.github.com>
2019-11-03 13:52:14 +0530

    GAME COMPLETE
    If you want to play game go to run folder and play and if you edit game go to run2 folder
    The pause menu is not working properly else it is fine

--- Zombie_Chronicles/Assets/scripts/PauseMenu.cs
+++ Zombie_Chronicles/Assets/scripts/PauseMenu.cs
@@ -45,7 +45,7 @@ public class PauseMenu : MonoBehaviour
     public void Restart()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Tutorial");
+        SceneManager.LoadScene("Level1");
     }
 
     public void Menu()
